3b4d88591f5567fc78e8d329db3ec0447e589a49
Stephan Meesters <stephan.meesters@gmail.com>
2025-08-22 14:13:43 +0200

    Add fabric dependency

--- files/scripts/install-modus-theme.sh
+++ files/scripts/install-modus-theme.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 
 git clone https://github.com/S4NKALP/Modus /usr/share/Modus
 cd /usr/share/Modus && git checkout 316e3066ba2427b687bde3381642b4bf10caa222
+
+pip install --no-deps git+https://github.com/Fabric-Development/fabric.git
