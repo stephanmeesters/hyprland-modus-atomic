425b3e475ae09c6c1fac3c15efd9b7ea12e47056
Stephan Meesters <stephan.meesters@gmail.com>
2025-08-22 22:53:19 +0200

    Add venv

--- files/scripts/install-modus-theme.sh
+++ files/scripts/install-modus-theme.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 git clone https://github.com/S4NKALP/Modus /usr/share/Modus
 cd /usr/share/Modus && git checkout 316e3066ba2427b687bde3381642b4bf10caa222
 
-pip install --no-deps git+https://github.com/Fabric-Development/fabric.git
+cd /usr/share/Modus \
+  && python -m venv .venv \
+  && source .venv/bin/activate \
+  && pip install -r requirements.txt \
+  && pip install --no-deps git+https://github.com/Fabric-Development/fabric.git
