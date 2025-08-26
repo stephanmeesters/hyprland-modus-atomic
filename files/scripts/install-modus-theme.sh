cf15742ef7558fb6a4a4963cf19ed6c01dd3fc2f
Stephan Meesters <stephan.meesters@gmail.com>
2025-08-26 13:22:29 +0200

    Add custom hyprland config file

--- files/scripts/install-modus-theme.sh
+++ files/scripts/install-modus-theme.sh
@@ -4,8 +4,5 @@ set -euo pipefail
 git clone https://github.com/stephanmeesters/Modus /usr/share/Modus
 # cd /usr/share/Modus && git checkout 316e3066ba2427b687bde3381642b4bf10caa222
 
-# cd /usr/share/Modus \
-  # && python -m venv .venv \
-  # && source .venv/bin/activate \
-  # && pip install -r requirements.txt \
 pip install --no-deps git+https://github.com/Fabric-Development/fabric.git
+pip install thefuzz pyzbar
